53360115b7b69032d01b6af36e4b5df47d4e803f
Marc King <mdkmba@gmail.com>
2017-07-26 22:30:16 -0400

    added ArrayList complete declaration to CarLot Class for calling from outside the class

--- src/CarLot.java
+++ src/CarLot.java
@@ -1,41 +1,63 @@
-import java.util.ArrayList;
+import java.util.*;
+import java.util.Scanner;
+import java.util.stream.Stream;
 
 public class CarLot {
-    int selection;
-    private ArrayList<Car> carArrayListM;
 
-    public CarLot(ArrayList<Car> carArrayList) {
-        carArrayListM = carArrayList;
+    int i = 0;
+
+    Scanner scnr = new Scanner (System.in);
+    public static ArrayList<Car> list = new ArrayList<Car>();
+
+    {
+        list.add(new UsedCar("BMW", "328i", 2011, 11000, 109000));
+        list.add(new Car("Ford", "Fusion", 2017, 28000));
+        list.add(new UsedCar("GM", "Corvette", 2015, 30000, 78000));
+        list.add(new Car("Lexus", "GX 460", 2017, 66000));
+        list.add(new UsedCar("Mercedes", "CL500", 2001, 18000, 160000));
+        list.add(new Car("Ford", "Mustang", 2017, 35000));
+        list.add(new UsedCar("BMW", "740", 2010, 19000, 152000));
+        list.add(new UsedCar("Ford", "Pinto", 1974, 2.63, 732.9));
     }
 
+    public static ArrayList<Car> getList() {
+        return list;
+    }
 
-    public void listall() {
-
-        for (int i = 0; i < carArrayListM.size(); i++) {
+    public String listall() {
+        int i = 0;
+        for (i = 0; i < list.size(); i++) {
             System.out.print((i + 1) + ". ");
-            System.out.println(carArrayListM.get(i));
+            System.out.println(list.get(i));
         }
+        return String.valueOf(i);
     }
 
     public void addCar(Car addCar) {
 
-        carArrayListM.add(addCar);
+        list.add(addCar);
     }
 
-    public void deleteCar(selection) {
+    public void deleteCar() {
 
-        carArrayListM.remove(selection);
+        list.remove(i);
     }
 
     public void infoCar() {
 
-        System.out.print(carArrayListM.get(selection));
+        System.out.print(list.get(i));
     }
 
-    public void carSwap() {
-
-        carArrayListM.remove(selection);
-        carArrayListM.add(selection, swap);
+    public void addcar () {
+        list.add(new Car());
+        System.out.print("\nEnter Make: ");
+        list.get(i).setMakeM(scnr.nextLine());
+        System.out.print("\nEnter Model: ");
+        list.get(i).setModelM(scnr.nextLine());
+        System.out.print("\nEnter Year: ");
+        list.get(i).setYearM(scnr.nextInt());
+        System.out.print("\nEnter Price: ");
+        list.get(i).setPriceM(scnr.nextDouble());
+        scnr.nextLine();
     }
-
 }
--- src/CarLotApp.java
+++ src/CarLotApp.java
@@ -1,72 +1,52 @@
+import java.io.PrintStream;
 import java.util.*;
 
-/**
- * Created by marcking2 on 7/24/17.
- */
-public class CarLotApp {
+class CarLotApp {
 
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
         int selection = 0;
+        boolean loop = true;
+        CarLot x = new CarLot();
+//
 
-        ArrayList<Car> carArrayList;
-        carArrayList = new ArrayList<>();
-
-        carArrayList.add(new UsedCar("BMW", "328i", 2011, 11000, 109000));
-        carArrayList.add(new Car("Ford", "Fusion", 2017, 28000));
-        carArrayList.add(new UsedCar("GM", "Corvette", 2015, 30000, 78000));
-        carArrayList.add(new Car("Lexus", "GX 460", 2017, 66000));
-        carArrayList.add(new UsedCar("Mercedes", "CL500", 2001, 18000, 160000));
-        carArrayList.add(new Car("Ford", "Mustang", 2017, 35000));
-        carArrayList.add(new UsedCar("BMW", "740", 2010, 19000, 152000));
-        carArrayList.add(new UsedCar("Ford", "Pinto", 1974, 2.63, 732.9));
+        int userSelect = 0;
 
 
+        System.out.print("\n\n\nWelcome to King Cisero New & Used Cars!\n");
 
-        System.out.print("\n\n\nWelcome to King Cisero New & Used Cars!");
 
+        do {
+            System.out.println("\nCurrent Inventory:");
+            System.out.printf("\n%-2s %-12s%-12s%-12s%-18s%-18s\n", "#", "Make", "Model", "Year", "Price", "Mileage(Used Cars)");
+            System.out.printf("%-2s %-12s%-12s%-12s%-18s%-18s\n", "==", "==========", "==========", "==========", "==============", "==================");
+            System.out.println();
+            x.listall();
+            System.out.println("\n1. List of available inventory.\n2. Add a car to inventory." +
+                    "\n3. Remove a car from inventory.\n4. Select a car to review.\n5. Replace a car in inventory.\n\n");
+            System.out.print("Please make a selection: ");
+            userSelect = scnr.nextInt();
 
-        do{
-        System.out.println("\nCurrent Inventory:");
-        System.out.printf("\n%-2s %-12s%-12s%-12s%-18s%-18s\n", "#", "Make", "Model", "Year", "Price", "Mileage(Used Cars)");
-        System.out.printf("%-2s %-12s%-12s%-12s%-18s%-18s\n", "==", "==========", "==========", "==========", "==============", "==================");
+            switch (userSelect) {
+                case 1:
+                    x.listall();
+//                case 2:
+//                    x.addCar();
+                case 3:
+                    x.deleteCar();
+                case 4:
+                    x.infoCar();
+                case 5:
+                    x.deleteCar();
+                case 6:
+                    loop = false;
 
-        CarLot carList = new CarLot(carArrayList);
-            carList.listall();
-            carList.addCar();
-            carList.deleteCar();
-            carList.infoCar();
-            carList.carSwap();
-
-            public void infoCar() {
-            System.out.print("\nWhich car would you like:  ");
-            selection = (scnr.nextInt() - 1);
-
-        }
-        public void addcar() {
-            for (int i = 0; i < n; i++) {
-                carList.add(new Car());
-                System.out.print("\nEnter Make: ");
-                carArrayList.get(i).setMakeM(scnr.nextLine());
-                System.out.print("\nEnter Model: ");
-                carArrayList.get(i).setModelM(scnr.nextLine());
-                System.out.print("\nEnter Year: ");
-                carArrayList.get(i).setYearM(scnr.nextInt());
-                System.out.print("\nEnter Price: ");
-                carArrayList.get(i).setPriceM(scnr.nextDouble());
-                scnr.nextLine();
+                default:
+                    userSelect = 1;
             }
 
-        }
-            }
-
-
-
-
-        }
-
+        } while (loop);
 
 
     }
-
-
+}
